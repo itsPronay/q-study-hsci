01b11a1dc6218fa5e985a86fecdad8acdc390073
itsPronay <pronaycoding@gmail.com>
2026-06-08 06:15:28 +0600

    Print layers?

--- spectralSpacialMamba/quantize_mamba.py
+++ spectralSpacialMamba/quantize_mamba.py
@@ -2,9 +2,25 @@ import torch
 from hqq.core.quantize import BaseQuantizeConfig
 import hqq_wrapper
 from spectralSpacialMamba.utils import test_batch
+import numpy as np
+
+
+def getParamCount(model, printLayers=False):
+    total_param = 0
+    for name, param in model.named_parameters():
+        if param.requires_grad:
+            num_param = np.prod(param.size())
+            if param.dim() > 1 and printLayers:
+                print(name+':', 'x'.join(str(x) for x in list(param.size())), '=', num_param)
+            elif printLayers:
+                print(name+':', num_param)
+            total_param += num_param
     
+    print("\nTotal Trainable Parameters:", total_param)
+    return total_param
 
 def test_batch_quantized(args, model, image, index, BATCH_SIZE, nTrain_perClass, nvalid_perClass, halfsize,):
+    getParamCount(model, printLayers=True)
         
     quant_config = BaseQuantizeConfig(
         nbits=args.nbits,
