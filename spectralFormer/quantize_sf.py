f34e512d11f2fdfe5b44a1b1c790aa1717c8e23a
itsPronay <pronaycoding@gmail.com>
2026-06-10 04:20:42 +0600

    update

--- spectralFormer/quantize_sf.py
+++ spectralFormer/quantize_sf.py
@@ -9,7 +9,7 @@ from utils.get_model_summary import getParamCount, printWeightStatistics, print_
 def test_batch_quantized(args, model):
     exclude_layers = [
         "mlp_head",
-        "patch_to_embedding" # fails for group_size = 64
+        # "patch_to_embedding" # fails for group_size = 64
     ]
 
     # getParamCount(model, printLayers=True)        
