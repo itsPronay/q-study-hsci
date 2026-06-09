e60a60175d4e41ec370a06fd9badb07491ab1eea
itsPronay <pronaycoding@gmail.com>
2026-06-09 18:27:09 +0600

    fixed args

--- spectralSpacialMamba/run.py
+++ spectralSpacialMamba/run.py
@@ -29,65 +29,42 @@ def run(args):
     day = datetime.datetime.now()
     day_str = day.strftime('%m_%d_%H_%M')
 
-    num_of_ex = args.train_loop
-    windowsize = args.windowsize
-    type = args.type
-
-    train_num = args.train_num
-
-    lr = args.lr
-    batch_size = args.batch_size
-    drop_rate = args.drop_rate
-    gamma = args.lr_decay
-
-    model_id = args.model_id
-
-    spe_windowsize = args.spe_windowsize
-    spa_patch_size = args.spa_patch_size
-    spe_patch_size = args.spe_patch_size
-    embed_dim = args.embed_dim
-    hid_chans = args.hid_chans
-    depth = args.depth
-    use_bi = args.use_bi
-    use_global = args.use_global
-    use_cls = args.use_cls
-
-    halfsize = int((windowsize-1)/2)
+    halfsize = int((args.windowsize-1)/2)
     train_image, train_label, validation_image, validation_label,nTrain_perClass, nvalid_perClass, index,image, gt,s = readdata(args, 0)
     gt = gt.astype(np.int32)
     nclass = np.max(gt)
-    result = np.zeros([nclass+3, num_of_ex])
+    result = np.zeros([nclass+3, args.train_loop])
     nband = train_image.shape[-1]
 
 
     net_name_candidate = ['mamba_1D_model', 'mamba_2D_model', 'mamba_SS_model']
-    net_name = net_name_candidate[model_id]
+    net_name = net_name_candidate[args.model_id]
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     ce_loss = torch.nn.CrossEntropyLoss()
     AC, OA, AA, KA, CM,  TRAINING_TIME, TESTING_TIME = [], [], [], [], [], [], []
 
-    for num in range(0, num_of_ex):
+    for num in range(0, args.train_loop):
         print('num:', num)
         train_image, train_label, validation_image, validation_label,nTrain_perClass, nvalid_perClass, index,image, gt,s = readdata(args, num)
         train_image = np.transpose(train_image,(0,3,1,2))
         validation_image = np.transpose(validation_image,(0,3,1,2))
         nvalid_perClass = np.zeros_like(nvalid_perClass)
         
-        if model_id == 0:
-            model = mamba_1D_model(img_size=(spe_windowsize,spe_windowsize), spa_img_size=(windowsize, windowsize), nband=nband, patch_size=spe_patch_size, embed_dim=embed_dim, nclass=nclass, depth=depth, bi=use_bi, norm_layer=nn.LayerNorm, global_pool=use_global, cls = use_cls)
-        elif model_id == 1:
-            model = mamba_2D_model(img_size=(windowsize, windowsize), patch_size=spa_patch_size, in_chans=nband, hid_chans = hid_chans, embed_dim=embed_dim, nclass=nclass, drop_path=drop_rate, depth=4, bi=use_bi, norm_layer=nn.LayerNorm, global_pool=use_global, cls = use_cls)
-        elif model_id == 2:  
-            model = mamba_SS_model(spa_img_size=(windowsize, windowsize),spe_img_size=(spe_windowsize,spe_windowsize), spa_patch_size=spa_patch_size, spe_patch_size=spe_patch_size, in_chans=nband, hid_chans = hid_chans, embed_dim=embed_dim, drop_path=drop_rate, nclass=nclass, depth=depth, bi=use_bi, norm_layer=nn.LayerNorm, global_pool=use_global, cls = use_cls, fu = args.use_fu)
+        if args.model_id == 0:
+            model = mamba_1D_model(img_size=(args.spe_windowsize,args.spe_windowsize), spa_img_size=(args.windowsize, args.windowsize), nband=nband, patch_size=args.spe_patch_size, embed_dim=args.embed_dim, nclass=nclass, depth=args.depth, bi=args.use_bi, norm_layer=nn.LayerNorm, global_pool=args.use_global, cls = args.use_cls)
+        elif args.model_id == 1:
+            model = mamba_2D_model(img_size=(args.windowsize, args.windowsize), patch_size=args.spa_patch_size, in_chans=nband, hid_chans = args.hid_chans, embed_dim=args.embed_dim, nclass=nclass, drop_path=args.drop_rate, depth=4, bi=args.use_bi, norm_layer=nn.LayerNorm, global_pool=args.use_global, cls = args.use_cls)
+        elif args.model_id == 2:  
+            model = mamba_SS_model(spa_img_size=(args.windowsize,args. windowsize),spe_img_size=(args.spe_windowsize,args.spe_windowsize), spa_patch_size=args.spa_patch_size, spe_patch_size=args.spe_patch_size, in_chans=nband, hid_chans = args.hid_chans, embed_dim=args.embed_dim, drop_path=args.drop_rate, nclass=nclass, depth=args.depth, bi=args.use_bi, norm_layer=nn.LayerNorm, global_pool=args.use_global, cls = args.use_cls, fu = args.use_fu)
         else:
             raise Exception('model id does not find')
         model.to(device)
-        optimizer = optim.Adam(model.parameters(),lr = lr, weight_decay = 1e-4)
+        optimizer = optim.Adam(model.parameters(),lr = args.lr, weight_decay = 1e-4)
         print('the number of training samples:', train_image.shape[0])
 
         train_dataset = TensorDataset(torch.tensor(train_image), torch.tensor(train_label))
-        train_loader = DataLoader(dataset=train_dataset, batch_size= batch_size, shuffle=True, drop_last=False)
-        scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer,  milestones = [80, 140, 170], gamma = gamma, last_epoch=-1)
+        train_loader = DataLoader(dataset=train_dataset, batch_size= args.batch_size, shuffle=True, drop_last=False)
+        scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer,  milestones = [80, 140, 170], gamma = args.lr_decay, last_epoch=-1)
         # scheduler = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(optimizer,  T_0=5,T_mult=2)
         # training
     
@@ -143,9 +120,9 @@ def run(args):
     
     if not os.path.exists('record'):
         os.makedirs('record')
-    np.save('record/'+ net_name +'_'+ day_str + '_' +args.dataset+'_'+str(train_image.shape[0]) + '_epoch_' + str(args.epoch) + '_spa_patch_size_' + str(spa_patch_size) +'_spe_patch_size_'+str(spe_patch_size) + '_embed_dim_'+str(embed_dim)+'_depth _'+str(depth)+ '_use_global_'+str(use_global) +'_use_bi _'+str(use_bi)+'_hdi_chans_'+str(hid_chans)+'_lr _'+str(lr)+'_lrdecay_'+str(gamma)+ '_fusion_'+str(args.use_fu) +'_.npy', result)
+    np.save('record/'+ net_name +'_'+ day_str + '_' +args.dataset+'_'+str(train_image.shape[0]) + '_epoch_' + str(args.epoch) + '_spa_patch_size_' + str(args.spa_patch_size) +'_spe_patch_size_'+str(args.spe_patch_size) + '_embed_dim_'+str(args.embed_dim)+'_depth _'+str(args.depth)+ '_use_global_'+str(args.use_global) +'_use_bi _'+str(args.use_bi)+'_hdi_chans_'+str(args.hid_chans)+'_lr _'+str(args.lr)+'_lrdecay_'+str(args.lr_decay)+ '_fusion_'+str(args.use_fu) +'_.npy', result)
         
-    record_output(OA, AA, KA, ELEMENT_ACC, CM, TRAINING_TIME, TESTING_TIME, './record/' + net_name +'_'+ day_str + '_' +args.dataset+'_'+str(train_image.shape[0])+ '_epoch_' + str(args.epoch)+ '_spa_patch_size_' +str(spa_patch_size) +'_spe_patch_size_'+str(spe_patch_size) + '_embed_dim_'+str(embed_dim)+'_depth _'+str(depth)+ '_use_global_'+str(use_global) +'_use_bi _'+str(use_bi)+'_hdi_chans_'+str(hid_chans)+'_lr _'+str(lr)+'_lrdecay_'+str(gamma)+ '_fusion_'+str(args.use_fu) + '_end.txt') 
+    record_output(OA, AA, KA, ELEMENT_ACC, CM, TRAINING_TIME, TESTING_TIME, './record/' + net_name +'_'+ day_str + '_' +args.dataset+'_'+str(train_image.shape[0])+ '_epoch_' + str(args.epoch)+ '_spa_patch_size_' +str(args.spa_patch_size) +'_spe_patch_size_'+str(args.spe_patch_size) + '_embed_dim_'+str(args.embed_dim)+'_depth _'+str(args.depth)+ '_use_global_'+str(args.use_global) +'_use_bi _'+str(args.use_bi)+'_hdi_chans_'+str(args.hid_chans)+'_lr _'+str(args.lr)+'_lrdecay_'+str(args.lr_decay)+ '_fusion_'+str(args.use_fu) + '_end.txt') 
     
     output = {
         'model' : 'SpectralSpacialMamba',
