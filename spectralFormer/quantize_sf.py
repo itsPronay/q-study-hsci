dbf96b38817df64edfe6f2c934a48c1dffcbba74
itsPronay <pronaycoding@gmail.com>
2026-06-11 19:21:12 +0600

    removed net.0 from the list

--- spectralFormer/quantize_sf.py
+++ spectralFormer/quantize_sf.py
@@ -14,9 +14,9 @@ def test_batch_quantized(args, model):
 
     exclude_layers = [
         "mlp_head",
-        "patch_to_embedding" # fails for group_size = 64
-        "net.0",               # FFN layer 1 - too small for group_size=512
-        "net.3",               # FFN layer 2 - too small for group_size=512
+        "patch_to_embedding", # fails for group_size = 64
+        # "net.0",               # FFN layer 1 - too small for group_size=512
+        # "net.3",               # FFN layer 2 - too small for group_size=512
     ]
 
     model = hqq_wrapper.replace_all_linear_with_hqq_safe(
