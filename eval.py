88e49a7bc695620760f5d70eeb620519fc94f1ea
itsPronay <pronaycoding@gmail.com>
2026-06-22 12:48:46 +0600

    Update

--- eval.py
+++ eval.py
@@ -35,7 +35,7 @@ def get_args():
 
     # wandb 
     parser.add_argument("--wandb_mode", default="online", choices=["online", "offline", "disabled"])
-    parser.add_argument('--wandb_project', type=str, default='QHSIC_final_benchmark', help='wandb project name')
+    parser.add_argument('--wandb_project', type=str, default='QHSIC_final_study', help='wandb project name')
 
     args = parser.parse_args()
     return args
