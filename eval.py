23e44bb04bdd7519d95e828dd3c6ca5e452d6152
itsPronay <pronaycoding@gmail.com>
2026-06-30 20:20:19 +0600

    update

--- eval.py
+++ eval.py
@@ -148,7 +148,7 @@ def main():
     clas_acc_quantized = class_accuracy_percent(test_tar_quantized, test_pre_quantized, num_classes)
 
     compare_cka_and_print_result(
-        layer_name = args.exclude_layers,
+        layer_name = args.exclude_layers[0],
         model = model,
         quantized_model = quantized_model,
         test_loader = test_loader,
